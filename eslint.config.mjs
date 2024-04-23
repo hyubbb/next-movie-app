c8d8343d880152b8ab6b9ca5e23057be2061fb1e
hyubbb <dlckdguq1011@gmail.com>
2024-04-24 04:07:59 +0900

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,27 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-export default [
-  // {languageOptions: { globals: globals.browser }},
-  // pluginJs.configs.recommended,
-  // ...tseslint.configs.recommended,
-  // pluginReactConfig,
-];
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    pluginJs.configs.recommended,
+    ...tslint.configs.recommended,
+    pluginReactConfig,
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "no-unused-vars": "warn",
+    "react/no-find-dom-node": "off",
+  },
+  globals: globals.browser, // 'globals' 설정 추가
+};
