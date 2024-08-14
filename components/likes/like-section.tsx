519f7fb3ad17480180e928f018faf2943c047cda
hyubbb <dlckdguq1011@gmail.com>
2024-08-14 13:27:40 +0900

    _

--- components/likes/like-section.tsx
+++ components/likes/like-section.tsx
@@ -3,22 +3,25 @@ import LikeCard from "./like-card";
 import Logout from "../navigation/logout";
 import LikeType from "./like-type";
 import { queryAll } from "../../actions/auth-actions";
-import { QueryClient, dehydrate } from "@tanstack/react-query";
 import { Suspense } from "react";
 import Spinner from "../commons/Spinner";
 import { prefetchAndDehydrate } from "../../lib/queryClient";
 
 export default async function LikeSection() {
-  const dehydratedState =
-    (await prefetchAndDehydrate(["likesPageData"], queryAll)) || {};
+  let dehydratedState;
+  try {
+    dehydratedState = await prefetchAndDehydrate(["likesPageData"], queryAll);
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+  }
 
   return (
     <div className={styles.container}>
       <Logout />
       <Suspense fallback={<Spinner />}>
         <LikeType query={dehydratedState} />
+        <LikeCard />
       </Suspense>
-      <LikeCard />
     </div>
   );
 }
