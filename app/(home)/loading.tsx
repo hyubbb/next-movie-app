15c798acbdf2ee229a537c8ca9f8519f2378eb6c
hyubbb <dlckdguq1011@gmail.com>
2024-10-12 00:27:17 +0900

    Fix : posterImage 로딩속도관련

--- app/(home)/loading.tsx
+++ app/(home)/loading.tsx
@@ -1,36 +1,39 @@
+import React from "react";
 import { Loader2 } from "lucide-react";
 
-const styles = {
-  loadingContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    position: "absolute",
-    width: "100vw",
-    height: "100vh",
-    zIndex: 100,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  spinner: {
-    margin: "1rem 0",
-    height: "1.75rem",
-    width: "1.75rem",
-    zIndex: 101,
-    color: "#fff", // zinc-500
-  },
-  loadingText: {
-    fontSize: "0.75rem",
-    zIndex: 102,
-    color: "#fff", // zinc-500
-  },
-};
-
-const Loading = () => {
+const Loading: React.FC = () => {
   return (
-    <div style={styles.loadingContainer}>
-      <Loader2 style={styles.spinner} />
-      <p style={styles.loadingText}>Loading...</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        width: "100vw",
+        height: "100vh",
+        zIndex: 100,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+      }}
+    >
+      <Loader2
+        style={{
+          margin: "1rem 0",
+          height: "1.75rem",
+          width: "1.75rem",
+          zIndex: 101,
+          color: "#fff",
+        }}
+      />
+      <p
+        style={{
+          fontSize: "0.75rem",
+          zIndex: 102,
+          color: "#fff",
+        }}
+      >
+        Loading...
+      </p>
     </div>
   );
 };
